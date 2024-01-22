0c45e92a84f23a363a5ab45f227c1046efda09c9
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-01-22 13:24:00 +0600

    Movie Details Modal

--- module-3-CineRental/src/components/Movie/MovieCard.jsx
+++ module-3-CineRental/src/components/Movie/MovieCard.jsx
@@ -1,31 +1,61 @@
+import { useState } from 'react';
 import { getImageUrl } from '../../utils/movie-utility';
+import MovieDetailsModal from './MovieDetailsModal';
 import Rating from './Rating';
 
 const MovieCard = ({ movie }) => {
-  const { id, cover, title, genre, rating, price } = movie;
+  const [showModal, setShowModal] = useState(false);
+  const { cover, title, genre, rating, price } = movie;
+
+  //* event handlers
+  const handleModalShow = () => {
+    setShowModal(true);
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
+  const handleAddToCart = (e, movie) => {
+    e.stopPropagation();
+    console.log(movie);
+  };
 
   return (
-    <figure className="p-4 border shadow-sm border-black/10 dark:border-white/10 rounded-xl">
-      <img
-        className="object-cover object-center w-full "
-        src={getImageUrl(cover)}
-        alt="movie-cover"
-      />
-      <figcaption className="pt-4">
-        <h3 className="mb-1 text-xl">{title}</h3>
-        <p className="text-[#575A6E] text-sm mb-2">{genre}</p>
-        <div className="flex items-center mb-5 space-x-1">
-          <Rating value={rating} />
-        </div>
-        <a
-          className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-          href="#"
-        >
-          <img src="./assets/tag.svg" alt="" />
-          <span>${price} | Add to Cart</span>
-        </a>
-      </figcaption>
-    </figure>
+    <>
+      {showModal && (
+        <MovieDetailsModal
+          movie={movie}
+          handleModalClose={handleModalClose}
+          handleAddToCart={handleAddToCart}
+        />
+      )}
+
+      <figure
+        onClick={handleModalShow}
+        className="p-4 border shadow-sm cursor-pointer border-black/10 dark:border-white/10 rounded-xl"
+      >
+        <img
+          className="object-cover object-center w-full "
+          src={getImageUrl(cover)}
+          alt="movie-cover"
+        />
+        <figcaption className="pt-4">
+          <h3 className="mb-1 text-xl">{title}</h3>
+          <p className="text-[#575A6E] text-sm mb-2">{genre}</p>
+          <div className="flex items-center mb-5 space-x-1">
+            <Rating value={rating} />
+          </div>
+          <a
+            onClick={(e) => handleAddToCart(e, movie)}
+            className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
+            href="#"
+          >
+            <span>${price} | Add to Cart</span>
+          </a>
+        </figcaption>
+      </figure>
+    </>
   );
 };
 
---
+++ module-3-CineRental/src/components/Movie/MovieDetailsModal.jsx
@@ -0,0 +1,50 @@
+import { getImageUrl } from '../../utils/movie-utility';
+
+const MovieDetailsModal = ({ movie, handleAddToCart, handleModalClose }) => {
+  const { cover, title, genre, description, price } = movie;
+
+  return (
+    <div className="fixed top-0 left-0 z-50 w-screen h-screen bg-black/60 backdrop-blur-sm">
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[420px] sm:max-w-[600px] lg:max-w-[984px] p-4 max-h-[90vh] overflow-auto">
+        <div className="bg-white shadow-md dark:bg-[#12141D] rounded-2xl sm:grid sm:grid-cols-[2fr_1fr] overflow-hidden">
+          <img
+            className="sm:order-2 w-full object-cover h-full max-sm:max-h-[300px]"
+            src={getImageUrl(cover)}
+            alt=""
+          />
+          {/* FIXME: need to change bg-color */}
+          <div className="p-5 bg-white lg:p-11">
+            <div className="">
+              <h2 className="text-3xl lg:text-[50px] mb-2 font-bold">
+                {title}
+              </h2>
+              <span className="block text-base text-[#9fa0a4] dark:text-[#575A6E] my-3">
+                {genre}
+              </span>
+              <div></div>
+            </div>
+            <p className="mb-8 text-sm lg:text-base lg:mb-16">{description}</p>
+            <div className="grid gap-2 lg:grid-cols-2">
+              <a
+                onClick={(e) => handleAddToCart(e, movie)}
+                className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
+                href="#"
+              >
+                <span>${price} | Add to Cart</span>
+              </a>
+              <a
+                onClick={handleModalClose}
+                className="border border-[#74766F] rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#4b4949] dark:text-gray-500 font-semibold text-sm"
+                href="#"
+              >
+                Cancel
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieDetailsModal;

ff09bef44a6fc941aa5900301769c130e65ad556
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-01-22 10:19:01 +0600

    project setup

---
+++ module-3-CineRental/src/shared/Header.jsx
@@ -0,0 +1,45 @@
+import logo from '../assets/icons/logo.svg';
+import ring from '../assets/icons/ring.svg';
+import shoppingCart from '../assets/icons/shopping-cart.svg';
+import sun from '../assets/icons/sun.svg';
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="container flex items-center justify-between py-6 space-x-10">
+        <a href="index.html">
+          <img src={logo} width="139" height="26" alt="" />
+        </a>
+
+        <ul className="flex items-center space-x-5">
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img src={ring} width="24" height="24" alt="" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img src={sun} width="24" height="24" alt="" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img src={shoppingCart} width="24" height="24" alt="" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;

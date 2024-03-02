e11fad907ddebb943f39198cefd9c44800a6e68a
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-03-02 19:12:17 +0600

    Project - FaceHook - Part 9 - Like and Comment Posts

--- module-5-facehook/src/components/posts/PostActions.jsx
+++ module-5-facehook/src/components/posts/PostActions.jsx
@@ -1,14 +1,44 @@
+import { useState } from 'react';
 import commentIcon from '../../assets/icons/comment.svg';
 import likeIcon from '../../assets/icons/like.svg';
+import likeFilledIcon from '../../assets/icons/likeFilled.svg';
 import shareIcon from '../../assets/icons/share.svg';
+import useAuth from '../../hooks/useAuth';
+import useAxios from '../../hooks/useAxios';
 
-const PostActions = ({ postId, commentCount }) => {
+const PostActions = ({ post, commentCount }) => {
+  const { auth } = useAuth();
+  const { api } = useAxios();
+  const [liked, setLiked] = useState(post?.likes?.includes(auth?.user?.id));
+
+  //* Handle Post Like
+  const handleLike = async () => {
+    try {
+      const response = await api.patch(
+        `${import.meta.env.VITE_SERVER_BASE_URL}/posts/${post?.id}/like`
+      );
+
+      if (response.status === 200) {
+        setLiked(true);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="flex items-center justify-between py-6 lg:px-10 lg:py-8">
       {/* Like Button  */}
-      <button className="flex-center gap-2 text-xs font-bold text-[#B8BBBF] hover:text-white lg:text-sm">
-        <img src={likeIcon} alt="Like" />
-        <span>Like</span>
+      <button
+        onClick={handleLike}
+        className="flex-center gap-2 text-xs font-bold text-[#B8BBBF] hover:text-white lg:text-sm"
+      >
+        <img
+          src={liked ? likeFilledIcon : likeIcon}
+          className="w-6"
+          alt="Like"
+        />
+        {<span> {liked ? 'Liked' : 'Like'}</span>}
       </button>
 
       {/* Comment Button  */}
--- module-5-facehook/src/components/posts/PostComments.jsx
+++ module-5-facehook/src/components/posts/PostComments.jsx
@@ -1,11 +1,36 @@
 import { useState } from 'react';
-import useAvatar from '../../hooks/useAvatar';
+import useAuth from '../../hooks/useAuth';
+import useAxios from '../../hooks/useAxios';
 import PostCommentList from './PostCommentList';
 
 const PostComments = ({ post }) => {
   const [showComments, setShowComments] = useState(true);
-  const { avatarURL } = useAvatar(post);
-  const { comments } = post || {};
+  const [comments, setComments] = useState(post?.comments);
+  const [comment, setComment] = useState('');
+  const { api } = useAxios();
+  const { auth } = useAuth();
+
+  //* Add New Comment
+  const addComment = async (e) => {
+    console.log('addComment fired');
+    const keyCode = e.keyCode;
+
+    if (keyCode === 13) {
+      try {
+        const response = await api.patch(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/posts/${post?.id}/comment`,
+          { comment }
+        );
+
+        if (response.status === 200) {
+          setComments([...response.data.comments]);
+          setComment('');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <div>
@@ -13,12 +38,15 @@ const PostComments = ({ post }) => {
       <div className="gap-2 mb-3 flex-center lg:gap-4">
         <img
           className="max-w-7 max-h-7 rounded-full lg:max-h-[34px] lg:max-w-[34px]"
-          src={avatarURL}
+          src={`${import.meta.env.VITE_SERVER_BASE_URL}/${auth?.user?.avatar}`}
           alt="avatar"
         />
 
         <div className="flex-1">
           <input
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            onKeyDown={(e) => addComment(e)}
             type="text"
             className="h-8 w-full rounded-full bg-lighterDark px-4 text-xs focus:outline-none sm:h-[38px]"
             name="post"
